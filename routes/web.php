a0dcbf34ed0a38529704ec830cc98b4eb5a230b0
WillP114 <125669038+89tuyenpq@users.noreply.github.com>
2024-02-22 15:38:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\ProductController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('admin')->group(function () {
